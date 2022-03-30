46f5a0dc540edc0b7b407aca45e39beaa67db3a0
Reshu Kumari <reshukumari786@gmail.com>
2022-03-31 00:17:38 +0530

    Time: 11 ms (96.24%), Space: 16.5 MB (97.03%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -3,6 +3,7 @@ public:
     int jump(vector<int>& nums) {
         int jumps = 0, curEnd = 0, curFarthest = 0;
         for (int i = 0; i < nums.size() - 1; i++) {
+            if (i > curFarthest) return -1;
             curFarthest = max(curFarthest, i + nums[i]);
             if (i == curEnd) {
                 jumps++;
