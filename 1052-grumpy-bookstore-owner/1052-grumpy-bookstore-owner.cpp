898bbaf15ce04beefb37b87ce8b6dc2cd9e0ebee
Reshu Kumari <reshukumari786@gmail.com>
2022-03-31 14:28:58 +0530

    Time: 36 ms (83.71%), Space: 32 MB (57.62%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         
-        int sum = 0, maxWindow = 0, window = 0;
+        /*int sum = 0, maxWindow = 0, window = 0;
         
         for (int i = 0; i < customers.size(); ++i) {
             sum += grumpy[i] == 0 ? customers[i] : 0;
@@ -11,6 +11,38 @@ public:
                 window -= grumpy[i - minutes] == 1 ? customers[i - minutes] : 0;
             maxWindow = max(maxWindow, window);
         }
-        return sum + maxWindow;
+        return sum + maxWindow;*/
+        int sum = 0;
+        for(int i=0;i<customers.size();i++){
+            if(grumpy[i] == 0){
+                sum += customers[i];
+            }
+        }
+        
+        int maxSum = sum;
+        for(int i=0;i<minutes;i++){
+            if(grumpy[i] == 1){
+                sum += customers[i];
+            }
+        }
+        if(sum > maxSum){
+            maxSum = sum;
+        }
+        int j = minutes;
+        int i=0;
+        while(j < customers.size()){
+            if(grumpy[j] == 1){
+                sum += customers[j];
+            }
+            if(grumpy[i] == 1){
+                sum -= customers[i];
+            }
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+            j++;
+            i++;
+        }
+        return maxSum;
     }
 };
