23340d76071078273d168b94c0ec918409358ef7
Reshu Kumari <reshukumari786@gmail.com>
2022-04-05 23:37:53 +0530

    Time: 213 ms (36.66%), Space: 61.4 MB (70.75%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -6,14 +6,7 @@ public:
         unordered_map<int, int> mp;
         while(j<n)
         {
-            if(mp.find(fruits[j])!=mp.end()) //already there in map
-            {
-                mp[fruits[j]]++;
-            }
-            else //not there in map
-            {
-                mp[fruits[j]]++;
-            }
+            mp[fruits[j]]++;
             while(mp.size()>2)
             {
                 mp[fruits[i]]--;
