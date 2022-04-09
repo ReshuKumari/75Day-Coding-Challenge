617b9303e3e678b6c0edee8299cc112d945a4a12
Reshu Kumari <reshukumari786@gmail.com>
2022-04-09 13:15:30 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (80.72%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<int> stk;
-        int l=0,r=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='(')
-            {
-                stk.push(i);
-                l++;
-            }
-            else if(!stk.empty()&& s[i]==')')
-            {    stk.pop(); l--;    }
+        int cnt = 0,ans = 0;
+        
+        for(int i =0;i<s.length();i++){
+            if(s[i]=='(')cnt++;
+            else cnt--;
+            if(cnt<0){ans+=abs(cnt);cnt=0;}
         }
-        int unmatchedl=s.size();
-        while(!stk.empty())
-            stk.pop();
-        for(int i=s.size()-1;i>=0;i--)
-        {
-            if(s[i]==')')
-            {
-                stk.push(i);
-                r++;
-            }
-            else if(!stk.empty()&& s[i]=='(')
-            {
-                stk.pop(); l--;
-            }
-                
-        }
-        int unmatchedr=stk.size();
-        //return unmatchedl+unmatchedr;
-        return l+r;
+        ans+=abs(cnt); 
+        
+        return ans;
     }
 };
